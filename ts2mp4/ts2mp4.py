bf702955252b8dc4128b3c5a7c23b87a4c609beb
dmingn <dmingn@users.noreply.github.com>
2025-07-05 10:03:18 +0900

    feat: Remove start and end time options
    This commit removes the command-line options `-ss` and `-to`, which allowed specifying start and end times for the conversion. The functionality has been removed from both the CLI and the core `ts2mp4` function to simplify the tool.

--- ts2mp4/ts2mp4.py
+++ ts2mp4/ts2mp4.py
@@ -1,31 +1,8 @@
-import re
 import subprocess
 from pathlib import Path
-from typing import Optional
 
 
-def _parse_duration_expression(dur_expr: str) -> float:
-    match = re.fullmatch(
-        r"(((?P<hour>\d+):)?(?P<minute>[0-5]?[0-9]):)?(?P<second>[0-5]?[0-9](\.(?P<microsecond>\d+))?)",
-        dur_expr,
-    )
-
-    if not match:
-        raise ValueError
-
-    return (
-        int(match.group("hour") or 0) * 3600
-        + int(match.group("minute") or 0) * 60
-        + float(match.group("second"))
-    )
-
-
-def ts2mp4(ts: Path, ss: Optional[str], to: Optional[str]):
-    ss_ = _parse_duration_expression(ss) if ss else None
-    to_ = _parse_duration_expression(to) if to else None
-    if ss_ and to_:
-        to_ -= ss_
-
+def ts2mp4(ts: Path):
     ts = ts.resolve()
     mp4 = ts.with_suffix(".mp4")
     mp4_part = ts.with_suffix(".mp4.part")
@@ -34,37 +11,29 @@ def ts2mp4(ts: Path, ss: Optional[str], to: Optional[str]):
         return
 
     proc = subprocess.run(
-        args=(
-            [
-                "ffmpeg",
-                "-fflags",
-                "+discardcorrupt",
-                "-y",
-            ]
-            + (["-ss", str(ss_)] if ss_ else [])
-            + [
-                "-i",
-                str(ts),
-                "-f",
-                "mp4",
-                "-vsync",
-                "1",
-                "-vf",
-                "bwdif",
-                "-codec:v",
-                "libx265",
-                "-crf",
-                "22",
-                "-codec:a",
-                "copy",
-                "-bsf:a",
-                "aac_adtstoasc",
-            ]
-            + (["-to", str(to_)] if to_ else [])
-            + [
-                str(mp4_part),
-            ]
-        )
+        args=[
+            "ffmpeg",
+            "-fflags",
+            "+discardcorrupt",
+            "-y",
+            "-i",
+            str(ts),
+            "-f",
+            "mp4",
+            "-vsync",
+            "1",
+            "-vf",
+            "bwdif",
+            "-codec:v",
+            "libx265",
+            "-crf",
+            "22",
+            "-codec:a",
+            "copy",
+            "-bsf:a",
+            "aac_adtstoasc",
+            str(mp4_part),
+        ]
     )
 
     if proc.returncode == 0:
