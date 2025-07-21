78e0efcc694474e4831c6e9acbfb2cd5ab91d333
dmingn <dmingn@users.noreply.github.com>
2025-07-21 11:58:14 +0900

    refactor: Remove redundant stream disabling argument in hashing.py

--- ts2mp4/hashing.py
+++ ts2mp4/hashing.py
@@ -26,10 +26,8 @@ def get_stream_md5(file_path: Path, stream: Stream) -> str:
     """
     if stream.codec_type == "audio":
         output_format = "s16le"
-        stream_disabling_arg = "-vn"  # Disable video stream
     elif stream.codec_type == "video":
         output_format = "rawvideo"
-        stream_disabling_arg = "-an"  # Disable audio stream
     else:
         raise ValueError(
             f"Unsupported stream type for MD5 calculation: {stream.codec_type}. "
@@ -40,7 +38,6 @@ def get_stream_md5(file_path: Path, stream: Stream) -> str:
         "-hide_banner",
         "-i",
         str(file_path),
-        stream_disabling_arg,
         "-map",
         f"0:{stream.index}",
         "-f",
